40bc2becb962b4ba8a5b49f059059d5381980744
dorcy <tugirimanadorcy64@gmail.com>
2022-05-26 19:14:33 -0500

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.styles.js'
+import JokesList from './components/Jokes/JokesList.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+  return(
+    <div style={styles.app}>
+      <JokesList></JokesList>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
